2b19dcda1148bf990aab96e3c004af327b43b6ae
BabySeeME「大雄」 <417170808@qq.com>
2022-01-06 16:41:12 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render(): React.ReactNode {
+    return (
+      <div className='App'>
+        this is a new world
+      </div>
+    )
+  }
+}
+
+export default App
