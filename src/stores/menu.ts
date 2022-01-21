4cf3be80803307e41c248582f792361d51375365
BabySeeME「大雄」 <417170808@qq.com>
2022-01-21 19:05:24 +0800

    add menuList storage

--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -1,3 +1,4 @@
+import { createStorage } from '@/utils/Storage'
 import { MenuDataItem } from '@ant-design/pro-layout'
 import { atom, useRecoilValue, RecoilValue, useRecoilState, RecoilState } from 'recoil'
 
@@ -6,21 +7,12 @@ export const menuListState = atom({
   default: []
 })
 
-export const setMenuState = (path: string, data: any) => {
-  console.log('path======', path)
-  console.log('path======', data)
-  const menuState = atom({
-    key: path,
-    default: null
-  })
-  const [_, setStateMenu] = useRecoilState(menuState)
-  setStateMenu(data)
+export const MenuListState = createStorage({ prefixKey: 'menuListState', storage: sessionStorage })
+
+export const setMenuListState = (path: string, data: any) => {
+  return MenuListState.set(path, data)
 }
 
-export const getMenuState = (path: string) => {
-  const menuState = atom({
-    key: path,
-    default: null
-  })
-  return useRecoilValue(menuState)
+export const getMenuListState = (path: string) => {
+  return MenuListState.get(path)
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,13 @@
-import Storage from './Storage'
+import Storage, { createStorage } from './Storage'
 
 export const Logged = () => {
   return Storage.get('accessToken') !== null ?? false
+}
+
+export const IsTopLevelMenu = () => {
+  const menuList = getMenuListState()
+  const currentMenu = menuList.filter((menu) => (
+    menu.path.toLowerCase() === location.pathname && menu?.children?.length
+  ))
+  return currentMenu.length > 0
 }
