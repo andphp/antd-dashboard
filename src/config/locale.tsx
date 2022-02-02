5ddd998f761bd3590c14b0aad790ac7f75bc6f28
DESKTOP-8GRFP91\admin <417170808@qq.com>
2022-02-02 23:44:16 +0800

    update icon

--- src/config/locale.tsx
+++ src/config/locale.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { ReactComponent as ZhCnSvg } from '@/assets/header/zh_CN.svg'
+import { ReactComponent as EnUsSvg } from '@/assets/header/en_US.svg'
+
 import enUS from '@/locales/en-us'
 import zhCN from '@/locales/zh-cn'
 
@@ -8,12 +11,12 @@ export const localeConfig = [
     name: 'English',
     key: 'en-us',
     messages: enUS,
-    icon: <>English</>
+    icon: <EnUsSvg />
   },
   {
     name: '简体中文',
     key: 'zh-cn',
     messages: zhCN,
-    icon: <>简体中文</>
+    icon: <ZhCnSvg />
   }
 ]
